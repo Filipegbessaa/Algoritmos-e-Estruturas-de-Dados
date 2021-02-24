0f1386ff5ddee1066facfd5ffca209b44409ad73
filipe Bessa <filipegbessa@gmail.com>
2021-02-24 12:56:31 -0300

    Add check_order method in BinaryTree class - Lista 2 - q1 test 2

--- Lista 2/q1.py
+++ Lista 2/q1.py
@@ -16,7 +16,7 @@ class BinaryTree:
 
         self.__first = None
         self.order = 1
-        self.current_node = self.__first
+        self.current_node = None
 
     def insert(self, value):
         parent = None
@@ -36,6 +36,7 @@ class BinaryTree:
             parent.left = Node(value)
             parent.left.parent = parent.data
             self.__first = Node(value)
+            self.current_node = self.__first
 
         else:
             parent.right = Node(value)
@@ -48,9 +49,33 @@ class BinaryTree:
         elif value > self.current_node.data:
             self.order += 1
 
-            if value < self.current_node.parent:
+            if self.current_node.parent == None:
+                self.order += 1
+                return self.order
+
+            elif value < self.current_node.parent:
                 return self.order
 
             elif value > self.current_node.parent:
                 self.current_node = self.current_node.parent
                 self.check_order(value)
+
+
+def main():
+    qty_numbers, qty_queries = input().split()
+    qty_numbers = int(qty_numbers)
+    qty_queries = int(qty_queries)
+    print(qty_numbers)
+    print(qty_queries)
+    tree = BinaryTree()
+    numbers_list = input().split()
+    for i in range(qty_numbers - 1, -1, -1):
+        tree.insert(int(numbers_list[i]))
+
+    for i in range(qty_queries):
+        number_to_check = input()
+        print(tree.check_order(int(number_to_check)))
+
+
+if __name__ == "__main__":
+    main()
